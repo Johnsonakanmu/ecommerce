54ae9e7a0d9fd5fb4b14fdee01d5bb75b449eb87
Johnson Akanmu <johnsonakanmu2017@gmail.com>
2024-09-07 10:20:55 +0100

    latest today

--- Dump20240907.sql
+++ Dump20240907.sql
@@ -16,6 +16,34 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `categories`
+--
+
+DROP TABLE IF EXISTS `categories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `categories` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `ProductName` varchar(255) DEFAULT NULL,
+  `description` text,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  `userId` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categories`
+--
+
+LOCK TABLES `categories` WRITE;
+/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
+INSERT INTO `categories` VALUES (7,'Jersey Men, women & Kids','Nice Jersey for Women and Kids','2024-09-06 09:06:43','2024-09-06 09:06:43',0),(8,'Cap and Hat','Nice Cap and Hat','2024-09-06 09:07:08','2024-09-06 09:07:08',0),(13,'Electronics Headphone','Nicre','2024-09-06 15:32:21','2024-09-06 15:32:21',2);
+/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `ordercarts`
 --
 
@@ -24,21 +52,18 @@ DROP TABLE IF EXISTS `ordercarts`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `ordercarts` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `userId` varchar(255) NOT NULL,
-  `productId` int NOT NULL,
+  `orderId` int NOT NULL,
   `imageUrl` varchar(255) DEFAULT NULL,
-  `productName` varchar(255) NOT NULL,
+  `productName` varchar(255) DEFAULT NULL,
   `size` enum('XS','S','M','XL','XXL','3XL') DEFAULT NULL,
   `color` enum('Dark','Yellow','White','Red','Green','Blue','Sky','Gray') DEFAULT NULL,
-  `productStock` int NOT NULL DEFAULT '0',
-  `productPrice` decimal(10,2) NOT NULL,
-  `amount` decimal(10,2) NOT NULL,
-  `orderId` int DEFAULT NULL,
+  `productStock` int DEFAULT '0',
+  `productPrice` decimal(10,2) DEFAULT NULL,
+  `amount` decimal(10,2) DEFAULT NULL,
   `createdAt` datetime NOT NULL,
   `updatedAt` datetime NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `orderId` (`orderId`),
-  CONSTRAINT `ordercarts_ibfk_1` FOREIGN KEY (`orderId`) REFERENCES `orders` (`id`)
+  `productId` int NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -60,11 +85,10 @@ DROP TABLE IF EXISTS `orders`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `orders` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `userId` varchar(255) NOT NULL,
-  `productId` varchar(255) NOT NULL,
+  `userId` int NOT NULL,
   `firstname` varchar(255) DEFAULT NULL,
   `lastname` varchar(255) DEFAULT NULL,
-  `email` varchar(255) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
   `phone` varchar(255) DEFAULT NULL,
   `address` varchar(255) DEFAULT NULL,
   `city` varchar(255) DEFAULT NULL,
@@ -85,7 +109,7 @@ CREATE TABLE `orders` (
 
 LOCK TABLES `orders` WRITE;
 /*!40000 ALTER TABLE `orders` DISABLE KEYS */;
-INSERT INTO `orders` VALUES (1,'1','3','John','Doe','john.doe@example.com','123-456-7890','123 Elm St','Abeokuta','Nigeria',12345,'credit Card','FedEx','2024-09-05 11:50:43','2024-09-05 11:50:43'),(2,'2','3','Jane','Smith','jane.smith@example.com','098-765-4321','456 Oak St','Ibadan','Nigeria',67890,'credit Card','DHL','2024-09-05 11:50:43','2024-09-05 11:50:43');
+INSERT INTO `orders` VALUES (1,1,'John','Doe','john.doe@example.com','123-456-7890','123 Elm St','Abeokuta','Nigeria',12345,'credit Card','FedEx','2024-09-05 11:50:43','2024-09-05 11:50:43'),(2,2,'Jane','Smith','jane.smith@example.com','098-765-4321','456 Oak St','Ibadan','Nigeria',67890,'credit Card','DHL','2024-09-05 11:50:43','2024-09-05 11:50:43');
 /*!40000 ALTER TABLE `orders` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -128,26 +152,25 @@ DROP TABLE IF EXISTS `products`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `products` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `productName` varchar(255) NOT NULL,
-  `productCategories` enum('Cloths','Jersey','Sportswear') NOT NULL,
+  `productName` varchar(255) DEFAULT NULL,
   `productBrand` varchar(255) DEFAULT NULL,
-  `productWeight` varchar(255) DEFAULT NULL,
   `gender` enum('Men','Women','Other') DEFAULT NULL,
   `size` enum('XS','S','M','XL','XXL','3XL') DEFAULT NULL,
   `color` enum('Dark','Yellow','White','Red','Green','Blue','Sky','Gray') DEFAULT NULL,
   `description` text,
   `tagNumber` int DEFAULT NULL,
-  `productStock` int NOT NULL DEFAULT '0',
-  `tag` enum('Fashion','Clothes','Jersey') DEFAULT NULL,
-  `productPrice` decimal(10,2) NOT NULL,
+  `productStock` int DEFAULT '0',
+  `productPrice` decimal(10,2) DEFAULT NULL,
   `productDiscount` float DEFAULT NULL,
   `productTax` float DEFAULT NULL,
   `imageUrl` varchar(255) DEFAULT NULL,
   `createdAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updatedAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `soldAmount` int DEFAULT NULL,
+  `categoryId` int NOT NULL,
+  `userId` int NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -156,7 +179,7 @@ CREATE TABLE `products` (
 
 LOCK TABLES `products` WRITE;
 /*!40000 ALTER TABLE `products` DISABLE KEYS */;
-INSERT INTO `products` VALUES (1,'Nigeria Jersey','Sportswear','Addidas','1','Men','M','White','Very good Jersey',78878,30,'Fashion',12.00,11,0,'image-1725532576899-nigeria-away jersey.jpeg','2024-09-05 10:36:16','2024-09-05 14:39:29',11),(2,'Nigeria Jersey','Jersey','NiKe','1','Men','S','Dark','Nice product',100009,100,'Fashion',30.00,25,8,'image-1725532688626-Nigeria j.jpeg','2024-09-05 10:38:08','2024-09-05 14:36:22',11),(3,'Tripple44fc','Cloths','Addidas','2kg','Other','M','Yellow','Nice',99991,9,'Fashion',40.00,30,0,'image-1725532766335-dolphins.jpg','2024-09-05 10:39:26','2024-09-05 14:36:31',1),(4,'3sc Jerser','Jersey','NiKe','1kg','Men','M','Yellow','Very nice Jersey',123,12,'Fashion',40.00,30,0,'image-1725540070432-Mens-New3.jpg','2024-09-05 12:41:10','2024-09-05 12:41:10',NULL),(5,'Tripple44fcs','Jersey','Addidas','2kg','Men','XS','Yellow','Good',1028787,12,'Clothes',30.00,23,5,'image-1725540666372-Mens-Jersey.jpg','2024-09-05 12:51:06','2024-09-05 13:34:40',NULL),(6,'Mens New Jersey','Cloths','Addidas','1','Other','M','Yellow','Nice one',90,12,'Fashion',20.00,10,2,'image-1725543912572-Mens-Kansas2.jpg','2024-09-05 13:45:12','2024-09-05 13:45:12',0),(7,'Dolpgin New Jersey','Sportswear','Addidas','1','Men','XS','Red','Nice one',90,6,'Fashion',30.00,29,2,'image-1725543998241-dolphins.jpg','2024-09-05 13:46:38','2024-09-05 14:52:43',1),(8,'Men New Jersey','Cloths','Addidas','1','Men','XS','Red','Nice one',90,6,'Fashion',15.00,13,2,'image-1725544055778-Mens-New3.jpg','2024-09-05 13:47:35','2024-09-05 14:52:29',1),(9,'Nigeria New Jersey','Jersey','Addidas','1','Other','XS','Red','Nice one',90,6,'Fashion',15.00,13,2,'image-1725544099191-Nigeria j.jpeg','2024-09-05 13:48:19','2024-09-05 13:48:19',0),(10,' Real Madira New Jersey','Cloths','Addidas','1','Women','XS','Red','Nice one',90,6,'Fashion',15.00,13,2,'image-1725544160185-jersy-3.jpeg','2024-09-05 13:49:20','2024-09-05 20:00:08',1),(12,' Man United New Jersey','Cloths','Addidas','1','Men','XXL','Yellow','Nice Jesery',90,6,'Fashion',15.00,13,2,'image-1725547816554-dolphins.jpg','2024-09-05 14:50:16','2024-09-05 14:50:16',0);
+INSERT INTO `products` VALUES (1,'Nigeria Jersey','Addidas','Men','M','White','Very good Jersey',78878,30,12.00,11,0,'image-1725532576899-nigeria-away jersey.jpeg','2024-09-05 10:36:16','2024-09-06 16:37:11',11,8,0),(2,'Nigeria Jersey','NiKe','Men','S','Dark','Nice product',100009,100,30.00,25,8,'image-1725532688626-Nigeria j.jpeg','2024-09-05 10:38:08','2024-09-05 14:36:22',11,0,0),(3,'Tripple44fc','Addidas','Other','M','Yellow','Nice',99991,9,40.00,30,0,'image-1725532766335-dolphins.jpg','2024-09-05 10:39:26','2024-09-05 14:36:31',1,0,0),(4,'3sc Jerser','NiKe','Men','M','Yellow','Very nice Jersey',123,12,40.00,30,0,'image-1725540070432-Mens-New3.jpg','2024-09-05 12:41:10','2024-09-05 12:41:10',NULL,0,0),(5,'Tripple44fcs','Addidas','Men','XS','Yellow','Good',1028787,12,30.00,23,5,'image-1725540666372-Mens-Jersey.jpg','2024-09-05 12:51:06','2024-09-05 13:34:40',NULL,0,0),(6,'Mens New Jersey','Addidas','Other','M','Yellow','Nice one',90,12,20.00,10,2,'image-1725543912572-Mens-Kansas2.jpg','2024-09-05 13:45:12','2024-09-05 13:45:12',0,0,0),(7,'Dolpgin New Jersey','Addidas','Men','XS','Red','Nice one',90,6,30.00,29,2,'image-1725543998241-dolphins.jpg','2024-09-05 13:46:38','2024-09-05 14:52:43',1,0,0),(8,'Men New Jersey','Addidas','Men','XS','Red','Nice one',90,6,15.00,13,2,'image-1725544055778-Mens-New3.jpg','2024-09-05 13:47:35','2024-09-05 14:52:29',1,0,0),(9,'Nigeria New Jersey','Addidas','Other','XS','Red','Nice one',90,6,15.00,13,2,'image-1725544099191-Nigeria j.jpeg','2024-09-05 13:48:19','2024-09-05 13:48:19',0,0,0),(10,' Real Madira New Jersey','Addidas','Women','XS','Red','Nice one',90,6,15.00,13,2,'image-1725544160185-jersy-3.jpeg','2024-09-05 13:49:20','2024-09-05 20:00:08',1,0,0),(12,' Man United New Jersey','Addidas','Men','XXL','Yellow','Nice Jesery',90,6,15.00,13,2,'image-1725547816554-dolphins.jpg','2024-09-05 14:50:16','2024-09-05 14:50:16',0,0,0),(13,'Niger Jersey','Addidas','Men','M','Yellow','Niger Jesery',9898,20,20.00,0,0,'image-1725616396964-Mens-Kansas2.jpg','2024-09-06 09:53:16','2024-09-06 09:53:16',0,7,0),(14,'Hat and Cap','Addidas','Men','M','White','Nice cap and hat',2232,12,12.00,11,0,'image-1725616715168-jersy-3.jpeg','2024-09-06 09:58:35','2024-09-06 09:58:35',0,8,0),(17,'Hat and Cap','Addidas','Men','XXL','White','Nice',2232,12,12.00,11,0,'image-1725623066012-Mens-New3.jpg','2024-09-06 11:44:26','2024-09-06 11:44:26',0,8,0),(18,'Tripple44fc','Addidas','Men','M','Yellow','Goood',34223,23,30.00,29,0,'image-1725625090254-Mens-Jersey.jpg','2024-09-06 12:18:10','2024-09-06 12:18:10',0,7,0),(19,'Tripple44fc','Addidas','Men','XXL','White','Goood',34223,23,30.00,29,0,'image-1725625249030-Mens-Jersey.jpg','2024-09-06 12:20:49','2024-09-06 12:20:49',0,7,0),(20,'Tripple44fc','Addidas','Men','M','Yellow','nice',34223,23,30.00,29,0,'image-1725625336969-Mens-New3.jpg','2024-09-06 12:22:17','2024-09-06 13:06:07',0,8,0),(21,'Nigeria Jersey','Addidas',NULL,'M','Dark','Good',29900,1,20.00,15,0,'image-1725641400786-Mens-New3.jpg','2024-09-06 16:50:00','2024-09-06 16:50:00',0,13,1);
 /*!40000 ALTER TABLE `products` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -180,7 +203,7 @@ CREATE TABLE `sequelizemeta` (
 
 LOCK TABLES `sequelizemeta` WRITE;
 /*!40000 ALTER TABLE `sequelizemeta` DISABLE KEYS */;
-INSERT INTO `sequelizemeta` VALUES ('20240828215540-create-product.js'),('20240830073033-create-user.js'),('20240830073438-create-payment.js'),('20240902104929-create-order.js'),('20240903133820-create-order-cart.js');
+INSERT INTO `sequelizemeta` VALUES ('20240828215540-create-product.js'),('20240830073033-create-user.js'),('20240830073438-create-payment.js'),('20240902104929-create-order.js'),('20240903133820-create-order-cart.js'),('20240906055807-create-category.js');
 /*!40000 ALTER TABLE `sequelizemeta` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -193,7 +216,7 @@ DROP TABLE IF EXISTS `users`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `users` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `sessionId` varchar(255) DEFAULT NULL,
+  `sessionId` int NOT NULL,
   `firstname` varchar(255) DEFAULT NULL,
   `lastname` varchar(255) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
@@ -218,7 +241,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,NULL,'John','Doe','john.doe@example.com','admin','123 Elm St','123-456-7890','Abeokuta','Nigeria',12345,'Ogun','true','2024-09-05 11:33:19','2024-09-05 11:33:19'),(2,NULL,'Jane','Smith','jane.smith@example.com','admin','456 Oak St','098-765-4321','Ibadan','Nigeria',67890,'Oyo','false','2024-09-05 11:33:19','2024-09-05 11:33:19');
+INSERT INTO `users` VALUES (1,1234,'John','Doe','john.doe@example.com','admin','123 Elm St','123-456-7890','Abeokuta','Nigeria',12345,'Ogun','true','2024-09-05 11:33:19','2024-09-05 22:06:59'),(2,12345,'Jane','Smith','jane.smith@example.com','admin','456 Oak St','098-765-4321','Ibadan','Nigeria',67890,'Oyo','false','2024-09-05 11:33:19','2024-09-05 22:06:59');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -231,4 +254,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-09-05 21:10:18
+-- Dump completed on 2024-09-07 10:16:30
